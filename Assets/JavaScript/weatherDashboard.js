fd4e6534bf5879cd4be044651e64a5b5d998a129
WickerPrison <cobylax17@gmail.com>
2024-02-01 11:31:19 -0700

    set up geocoding api

--- Assets/JavaScript/weatherDashboard.js
+++ Assets/JavaScript/weatherDashboard.js
@@ -3,13 +3,33 @@ var geocodingAPICall2 = "&limit=5&appid=d1978293ffe262f8e3bf58182e46f05c";
 
 var searchBtn = document.getElementById("search");
 var searchInput = document.getElementById("search-input");
+var results = document.getElementById("results");
+var todaysWeather = document.getElementById("todays-weather");
 
 function searchCity(){
     var fetchString = geocodingAPICall + searchInput.value + geocodingAPICall2;
-    fetch(fetchString){
-        
-    }
+    console.log(fetchString);
+    fetch(fetchString)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            results.innerHTML = "";
+            for(var i = 0; i < data.length;i++){
+                var resultEntry = document.createElement("li");
+                resultEntry.classList.add("result-button");
+                resultEntry.data = data[i];
+                resultEntry.innerText = data[i].name + ", " + data[i].state + ", " + data[i].country;
+                results.appendChild(resultEntry);
+                resultEntry.addEventListener("click", chooseCity)
+            }
+        });
+}
 
+function chooseCity(event){
+    var data = event.target.data;
+    var cityName = todaysWeather.querySelector("h1");
+    cityName.innerText = data.name + ", " + data.state;
 }
 
 searchBtn.addEventListener("click", searchCity);
