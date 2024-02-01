e0387f042b83a182eeadeb7f05a6ffbd2cfc2abe
WickerPrison <cobylax17@gmail.com>
2024-02-01 13:20:25 -0700

    set up previous serches from local storage

--- Assets/JavaScript/weatherDashboard.js
+++ Assets/JavaScript/weatherDashboard.js
@@ -5,7 +5,28 @@ var key = "d1978293ffe262f8e3bf58182e46f05c";
 var searchBtn = document.getElementById("search");
 var searchInput = document.getElementById("search-input");
 var results = document.getElementById("results");
+var previousSearches = document.getElementById("previous-searches");
 var todaysWeather = document.getElementById("todays-weather");
+
+var localStorageObject = JSON.parse(localStorage.getItem("previousSearches"));
+if(localStorageObject == null){
+    localStorageObject = [];
+}
+
+setupPreviousSearches();
+chooseCity(localStorageObject[0]);
+
+function setupPreviousSearches(){
+    previousSearches.innerHTML = "";
+    for(var i = 0; i < localStorageObject.length;i++){
+        var resultEntry = document.createElement("li");
+        resultEntry.classList.add("result-button");
+        resultEntry.data = localStorageObject[i];
+        resultEntry.innerText = localStorageObject[i].name + ", " + localStorageObject[i].state + ", " + localStorageObject[i].country;
+        previousSearches.appendChild(resultEntry);
+        resultEntry.addEventListener("click", chooseCityEvent);
+    }
+}
 
 function searchCity(){
     var fetchString = geocodingAPICall + searchInput.value + geocodingAPICall2 + key;
@@ -21,14 +42,20 @@ function searchCity(){
                 resultEntry.data = data[i];
                 resultEntry.innerText = data[i].name + ", " + data[i].state + ", " + data[i].country;
                 results.appendChild(resultEntry);
-                resultEntry.addEventListener("click", chooseCity)
+                resultEntry.addEventListener("click", chooseCityEvent);
             }
         });
 }
 
-function chooseCity(event){
-    var inputData = event.target.data;
-    console.log(inputData);
+function chooseCityEvent(event){
+    chooseCity(event.target.data);
+}
+
+function chooseCity(inputData){
+    localStorageObject.unshift(inputData);
+    saveToLocalStorage();
+    setupPreviousSearches();
+
     var cityName = todaysWeather.querySelector("h1");
     cityName.innerText = inputData.name + ", " + inputData.state;
 
@@ -44,5 +71,19 @@ function chooseCity(event){
     });
 }
 
+function saveToLocalStorage(){
+    var latArray = [];
+    localStorageObject.reverse();
+    for(var i = localStorageObject.length - 1; i >= 0; i--){
+        if(latArray.includes(localStorageObject[i].lat)){
+            localStorageObject.splice(i, 1);
+        }
+        else{
+            latArray.push(localStorageObject[i].lat);
+        }
+    }
+    localStorageObject.reverse();
+    localStorage.setItem("previousSearches", JSON.stringify(localStorageObject));
+}
 
 searchBtn.addEventListener("click", searchCity);
